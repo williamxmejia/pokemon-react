c2fcca8c147ff0402c65a36c7c2a41c48c04a577
williamxmejia <willx1995@gmail.com>
2022-11-03 14:49:41 -0500

    feat:style changes, moving everything to individual components

--- src/App.js
+++ src/App.js
@@ -1,77 +1,72 @@
-import Header from "./components/Header";
 import "./bootstrap.css";
-// import Api from "./components/PokemonApi";
 import Axios from "axios";
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css'
+import React, {useState, useEffect} from "react";
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const [filteredResuts, setFilteredResults] = useState([])
-  const [search, setSearch] = useState('');
+    const [pokemons, setPokemons] = useState([]);
+    const [filteredResuts, setFilteredResults] = useState([])
+    const [search, setSearch] = useState('');
 
-  const getPokemons = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/";
-    const response = await Axios(url);
-    const results = response.data.results;
+    const getPokemons = async () => {
+        const url = "https://pokeapi.co/api/v2/pokemon/?Limit=1000";
+        const response = await Axios(url);
+        const results = response.data.results;
+        console.log(url)
 
-    let pokemons = [];
+        let pokemons = [];
 
-    for (let result of results) {
-      const individualPokemonUrl = result.url;
-      const pokemonResponse = await Axios(individualPokemonUrl);
-      pokemons.push(pokemonResponse.data);
-    }
-    setPokemons(pokemons);
-  };
-
-  useEffect(() => {
-    getPokemons();
-  }, []);
-
-  useEffect(() => {
-    if(search !== ''){
-      const filteredPokemon = pokemons.filter((item) =>{
-        return item.name.includes(search.toLowerCase());
-      })
-      setFilteredResults(filteredPokemon)
-    }
-    else {
-      setFilteredResults(pokemons)
-    }
-  }, [search, pokemons])
-
-  return (
-    <div className="container">
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Header />}></Route>
-        </Routes>
-      </BrowserRouter> */}
-
-      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-
-      <div className="d-flex flex-wrap">
-        {
-            filteredResuts.map((x) =>{
-              return(
-                <div key={x.id} className="card mx-5 my-5">
-                  <img src={x.sprites.front_default}></img>
-                  <p className="text-center">{capitalizeFirstLetter(x.name)}</p>
-                </div>
-              )
-            })
+        for (let result of results) {
+            const individualPokemonUrl = result.url;
+            const pokemonResponse = await Axios(individualPokemonUrl);
+            pokemons.push(pokemonResponse.data);
         }
-      </div>
-    </div>
-  );
+        setPokemons(pokemons);
+    };
+
+    useEffect(() => {
+        getPokemons();
+    }, []);
+
+    useEffect(() => {
+        if (search !== '') {
+            const filteredPokemon = pokemons.filter((item) => {
+                return item.name.includes(search.toLowerCase());
+            })
+            setFilteredResults(filteredPokemon)
+        } else {
+            setFilteredResults(pokemons)
+        }
+    }, [search, pokemons])
+
+    return (
+        <div className="_abc">
+            <div className="navbar p-3">
+                <input className="mx-auto form-control" type="text" placeholder="Search Pokemon" value={search}
+                       onChange={(e) => setSearch(e.target.value)}/>
+            </div>
+
+            <div className="d-flex flex-wrap justify-content-center">
+                {
+                    filteredResuts.map((x) => {
+                        return (
+                            <div key={x.id} className="d-flex justify-content-center row mx-5 my-5 _d321">
+                                <img src={x.sprites.front_default}></img>
+                                <p className="text-center _b111">{capitalizeFirstLetter(x.name)}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-function test() {}
+function test() {
+}
 
 export default App;
---
+++ src/components/AllPoke.js
@@ -0,0 +1,11 @@
+
+
+function AllPoke (){
+    return(
+        <div>
+            <h1>Pokemon</h1>
+        </div>
+    )
+}
+
+export default AllPoke;
